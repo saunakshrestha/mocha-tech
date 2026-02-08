e50e9d393636d30eb7402e760f07be23d96d7f4a
saunakshrestha <sunk.stha@gmail.com>
2026-02-08 23:31:24 +0800

    Update favicon references in layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,9 +36,9 @@ export const metadata: Metadata = {
   publisher: "MochaTech Pty Ltd",
   icons: {
     icon: [
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicons/favicon.ico', sizes: 'any' },
     ],
     apple: [
       { url: '/favicons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
